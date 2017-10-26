67f6742a26a03d35a575ac95e088adca83e94e6f
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-10-26 09:39:53 +0200

    Boletin3_2

---
+++ Boletin3_2/src/boletin3_2/Boletin3_2.java
@@ -0,0 +1,17 @@
+
+package boletin3_2;
+
+/**
+ * @author jborrajorodriguez
+ */
+public class Boletin3_2 {
+
+   
+    public static void main(String[] args) {
+    Satelite satelite1=new Satelite();
+    satelite1.verPosicion();
+    Satelite sateliteConValores=new Satelite(123248,1231581,32188);
+    sateliteConValores.verPosicion();
+    }
+    
+}
